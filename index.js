5f616ac5e3c957c38c111bc5d1c550efebf2c84d
Prakash-8074 <janagamprakash28@gmail.com>
2024-12-21 22:06:55 +0530

    Uploaded all js and react programs of assignment 1

---
+++ index.js
@@ -0,0 +1,6 @@
+var hOneReact = React.createElement("h1", {}, "Hello World... React");
+
+
+var htmlRoot = document.getElementById("root");
+var rootReact = ReactDOM.createRoot(htmlRoot);
+rootReact.render(hOneReact);
